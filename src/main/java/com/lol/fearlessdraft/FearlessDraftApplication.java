534d4f10f9aeae31ddaf307dde59628f9f7381d2
naseokhoo <nahoo1202@naver.com>
2025-07-30 20:40:43 +0900

    first commit

---
+++ src/main/java/com/lol/fearlessdraft/FearlessDraftApplication.java
@@ -0,0 +1,13 @@
+package com.lol.fearlessdraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FearlessDraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FearlessDraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lol/fearlessdraft/FearlessDraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lol.fearlessdraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FearlessDraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
